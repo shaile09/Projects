d6542c77d26d2ce37e7badf581d6049657369aba
LISASHAH <57725386+LISASHAH@users.noreply.github.com>
2020-04-13 21:08:29 -0700

    Add files via upload

--- YelpReviewDataPredicts.py
+++ YelpReviewDataPredicts.py
@@ -1,39 +1,73 @@
 #%%
 #Import libraries
 import pandas as pd
-from pathlib import Path
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import psycopg2
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
+import tensorflow as tf
 
 # %%
 #Pull data from busiensses table from postgres
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost/Yelp')
 
 # %%
-# Check normal distribution of data using Shapiro-Wilk Test\
-
+ReviewsDF = pd.read_sql('select * from reviews',engine)
+ReviewsDF.head()
 # %%
-# Validate hypothesis using randam data generator
+# Generate our categorical variable list
+reviews_cat = ReviewsDF.dtypes[ReviewsDF.dtypes == "object"].index.tolist()
+ReviewsDF[reviews_cat].nunique()
+# %%
 
 # %%
 #Identify your label and features
+X = ReviewsDF.bussiness_id.values.reshape(-1, 1)
+X.shape
 
 # %%
-#Reshape your data
+y = ReviewsDF.stars
 
 # %%
-#Create modle using scikit-learb
+# Create a model with scikit-learn
+model = LinearRegression()
 
 # %%
-#Fit the model
+# Fit the data into the model
+model.fit(X, y)
 
 # %%
 # The model creates predicted y values based on X values
+y_pred = model.predict(X)
 
 # %%
-# Plot the results.
+#Get dataset ready for Database load
+Output_df= pd.DataFrame(y_pred)
+Output_df = Output_df.rename(columns={0:'RunPredition'})
+
+# %%
+X_df = pd.DataFrame(X)
+X_df = X_df.rename(columns={0:'Input'})
+X_df.head()
 
 
 # %%
-###Random Forest Classifier
+X_df['RunPredition']=Output_df['RunPredition']
+X_df.head()
 
 # %%
+from sqlalchemy.orm import Session
+session = Session(engine)
+#%%
+#Import data into postgres
+X_df.to_sql(name='output', con=engine, if_exists='replace' ,index=False)
+#%%
+print('Intercept: \n', model.intercept_)
+print('Coefficients: \n', model.coef_)
+
+#%%
+model.score(X,y)
